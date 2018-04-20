df6e20badcb763a5c537741ca2b7c12a676a47b9
Dino Puric <d.puric@sportradar.com>
2018-04-20 09:05:01 +0200

    added functionality to move slider with mouse events

--- main.js
+++ main.js
@@ -13,10 +13,17 @@ class CircleSlider {
 
         this.svgOptions = {
             width: 400,
-            stroke: 20,
+            strokeWidth: 20,
             dashStroke: '0.5%',
             strokeColor: 'black',
             circleBgOpacity: 0.1
+        }
+
+        this.sliderOptions = {
+            fill: 'orange',
+            // radius: this.options.radius / 4,
+            strokeColor: 'grey',
+            strokeWidth: 1
         }
 
         this.options = Object.assign({}, this.defaultOptions, options);
@@ -33,17 +40,17 @@ class CircleSlider {
             containerParent[0].appendChild(containerDiv);
         }
 
-        const svgElementId = this.defaultOptions.container + '-slider';
-        this.svgElem = document.getElementById(svgElementId);
-        if (!this.svgElem) {
-            this.svgElem = document.createElementNS(this.xmlns, 'svg');
-            this.svgElem.setAttribute('id', svgElementId);
-            this.svgElem.setAttribute('version', 1.1);
-            this.svgElem.setAttribute('width', this.svgOptions.width);
-            this.svgElem.setAttribute('height', this.svgOptions.width);
-            this.svgElem.setAttribute('viewPort', `${this.svgOptions.width}, ${this.svgOptions.width}`);
-            this.svgElem.style.margin = 'auto 0';
-            this.container.appendChild(this.svgElem);
+        var circleSvgId = this.defaultOptions.container + '-slider';
+        this.circleSvg = document.getElementById(circleSvgId);
+        if (!this.circleSvg) {
+            this.circleSvg = document.createElementNS(this.xmlns, 'svg');
+            this.circleSvg.setAttribute('id', circleSvgId);
+            this.circleSvg.setAttribute('version', 1.1);
+            this.circleSvg.setAttribute('width', this.svgOptions.width);
+            this.circleSvg.setAttribute('height', this.svgOptions.width);
+            this.circleSvg.setAttribute('viewPort', `${this.svgOptions.width}, ${this.svgOptions.width}`);
+            this.circleSvg.style.margin = 'auto 0';
+            this.container.appendChild(this.circleSvg);
         }
 
         this.render();
@@ -53,18 +60,85 @@ class CircleSlider {
         var circleSlider = document.createElementNS(this.xmlns, 'g');
 
         // Create circle
-        var circleSliderBG = document.createElementNS(this.xmlns, 'circle');
-        circleSliderBG.setAttribute('class', 'circle');
-        circleSliderBG.setAttribute('cx', this.svgOptions.width / 2);
-        circleSliderBG.setAttribute('cy', this.svgOptions.width / 2);
-        circleSliderBG.setAttribute('r', this.options.radius);
-        circleSliderBG.setAttribute('fill', 'none');
-        circleSliderBG.style.strokeWidth = this.svgOptions.stroke;
-        circleSliderBG.style.stroke = this.svgOptions.strokeColor;
-        circleSliderBG.style.opacity = this.svgOptions.circleBgOpacity;
-        circleSliderBG.setAttribute('stroke-dasharray', this.svgOptions.dashStroke);
-        circleSlider.appendChild(circleSliderBG);
-        this.svgElem.appendChild(circleSlider);
+        this.circleSliderBG = document.createElementNS(this.xmlns, 'circle');
+        this.circleSliderBG.setAttribute('class', 'circle');
+        this.circleSliderBG.setAttribute('cx', this.svgOptions.width / 2);
+        this.circleSliderBG.setAttribute('cy', this.svgOptions.width / 2);
+        this.circleSliderBG.setAttribute('r', this.options.radius);
+        this.circleSliderBG.setAttribute('fill', 'none');
+        this.circleSliderBG.style.strokeWidth = this.svgOptions.strokeWidth;
+        this.circleSliderBG.style.stroke = this.svgOptions.strokeColor;
+        this.circleSliderBG.style.opacity = this.svgOptions.circleBgOpacity;
+        this.circleSliderBG.setAttribute('stroke-dasharray', this.svgOptions.dashStroke);
+        circleSlider.appendChild(this.circleSliderBG);
+
+        this.mouseDownHandler = this.mouseDown.bind(this);
+        this.mouseUpHandler = this.mouseUp.bind(this);
+        this.mouseMoveHandler = this.mouseMove.bind(this);
+        this.circleSvg.addEventListener('mousedown', this.mouseDownHandler);
+
+        // Create slider
+        var sliderCoordinates = this.getPointOnCirle(3 * Math.PI / 2);
+        this.slider = document.createElementNS(this.xmlns, 'circle');
+        this.slider.setAttribute('r', this.svgOptions.strokeWidth / 2);
+        this.slider.setAttribute('cx', sliderCoordinates.x);
+        this.slider.setAttribute('cy', sliderCoordinates.y);
+        this.slider.style.fill = this.sliderOptions.fill;
+        this.slider.style.stroke = this.sliderOptions.strokeColor;
+        this.slider.style.strokeWidth = this.sliderOptions.strokeWidth;
+        circleSlider.appendChild(this.slider);
+
+        this.circleSvg.appendChild(circleSlider);
+
+    }
+
+    getPointOnCirle(angle) {
+        return {
+            x: Math.cos(angle) * this.options.radius + this.svgOptions.width / 2,
+            y: Math.sin(angle) * this.options.radius + this.svgOptions.width / 2
+        };
+    }
+
+    moveSlider(angle) {
+        const newPosition = this.getPointOnCirle(angle);
+        this.slider.setAttribute('cx', newPosition.x);
+        this.slider.setAttribute('cy', newPosition.y);
+    }
+
+    mouseDown(e) {
+        window.addEventListener('mousemove', this.mouseMoveHandler);
+        window.addEventListener('mouseup', this.mouseUpHandler);
+        var coords = {
+            x: e.clientX,
+            y: e.clientY
+        };
+        this.moveSlider(this.getAngle(coords));
+    }
+
+    mouseUp(e) {
+        window.removeEventListener('mousemove', this.mouseMoveHandler);
+        window.removeEventListener('mouseup', this.mouseUpHandler);
+        var coords = {
+            x: e.clientX,
+            y: e.clientY
+        };
+        this.moveSlider(this.getAngle(coords));
+    }
+
+    mouseMove(e) {
+        var coords = {
+            x: e.clientX,
+            y: e.clientY
+        };
+        this.moveSlider(this.getAngle(coords));
+    }
+
+    getAngle(coords) {
+        const rectangle = this.circleSliderBG.getBoundingClientRect();
+        var centerX = rectangle.x + (rectangle.width / 2);
+        var centerY = rectangle.y + (rectangle.height / 2);
+        var atan = -Math.atan2(coords.x-centerX, coords.y-centerY) + Math.PI / 2;
+        return atan;
     }
 }
 
